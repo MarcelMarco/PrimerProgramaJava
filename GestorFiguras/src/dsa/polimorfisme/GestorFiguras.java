f0d637a0b32a2c17ac0ad14eb3bda7618e62a76a
marioRelajao <mario.cerda@estudiantat.upc.edu>
2022-09-24 17:20:40 +0200

    Añadidas las funciones que restaban y añadidos algunos comentarios

--- GestorFiguras/src/dsa/polimorfisme/GestorFiguras.java
+++ GestorFiguras/src/dsa/polimorfisme/GestorFiguras.java
@@ -9,9 +9,26 @@ import dsa.polimorfisme.Rectangle;
 import dsa.polimorfisme.Triangle;
 
 public class GestorFiguras {
+
+    public static double suma (Figura[] v) { //Funcion retorna el sumatorio total de areas de la array
+        double sum = 0;
+        for (Figura f:v) {
+            sum = sum + f.area(); //Supongo que un sum+= f.area sirve, ala i++
+        }
+        return sum;
+    }
+    public static void sort(Figura[] v){ //Ordena en orden ascendente elementos comparables mutuamente(natural ordering)
+        Arrays.sort(v);
+    }
+
+    public static void print(Figura[] v){
+        for (Figura f:v){   //Por cada figura "f" dentro de v
+            System.out.println(f.getClass().getSimpleName() + " " + f.area());
+        }
+    }
     public static void main(String[] args) {
-        Figura[] v = new Figura[4];
-        Figura  r = new Rectangle(5,3);
+        Figura[] v = new Figura[4];  //Array de figuras  de long fija
+        Figura  r = new Rectangle(5,3);  //Definimos un rectangulo con lados 5 y 3, la clase se encarga de construirlo
         v[0] = r;
         v[1] = new Cercle(5);
         v[2] = new Quadrat(5);
@@ -21,11 +38,13 @@ public class GestorFiguras {
             System.out.println("El area de la figura "+i+" és : "+v[i].area());
         }
 
-        //double res = suma(v);
-        //System.out.println("suma: "+res);
-        //print(v);
-        //sort(v);
-        //print(v);
+        double res = suma(v);
+        System.out.println("suma: "+res);
+        print(v);
+        //System.out.println(v[0].getClass().getSimpleName()); //Imprimir si es rect, circulo etc
+        sort(v);
+        System.out.println("\nAhora la lista ordenada");
+        print(v);
         //System.out.println(v[0].area());
     }
 }
